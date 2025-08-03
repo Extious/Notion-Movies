79b7afd980acdfc58c92b5ef4cead8c1e47391b0
Zhao Zhan <zhaozhan.site@gmail.com>
2025-08-03 12:07:58 +0800

    Update blog configuration: change NOTION_PAGE_ID and THEME, clear BIO field

--- blog.config.js
+++ blog.config.js
@@ -4,8 +4,8 @@ const BLOG = {
   // Important page_id！！！Duplicate Template from  https://zhaozhan.notion.site/241c030b24dd80d08d3edcd1b7f4e371?v=241c030b24dd81649298000c3569d090&source=copy_link
   NOTION_PAGE_ID:
     process.env.NOTION_PAGE_ID ||
-    '241c030b24dd80d08d3edcd1b7f4e371',
-  THEME: process.env.NEXT_PUBLIC_THEME || 'hexo', // 当前主题，在themes文件夹下可找到所有支持的主题；主题名称就是文件夹名，例如 example,fukasawa,gitbook,heo,hexo,landing,matery,medium,next,nobelium,plog,simple
+    '244c030b24dd81cd9e18c16150ada269',
+  THEME: process.env.NEXT_PUBLIC_THEME || 'movie', // 当前主题，在themes文件夹下可找到所有支持的主题；主题名称就是文件夹名，例如 example,fukasawa,gitbook,heo,hexo,landing,matery,medium,next,nobelium,plog,simple
   LANG: process.env.NEXT_PUBLIC_LANG || 'zh-CN', // e.g 'zh-CN','en-US'  see /lib/lang.js for more.
   SINCE: process.env.NEXT_PUBLIC_SINCE || 2025, // e.g if leave this empty, current year will be used.
 
@@ -15,7 +15,7 @@ const BLOG = {
   APPEARANCE_DARK_TIME: process.env.NEXT_PUBLIC_APPEARANCE_DARK_TIME || [18, 6], // 夜间模式起至时间，false时关闭根据时间自动切换夜间模式
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'Extious', // 您的昵称
-  BIO: process.env.NEXT_PUBLIC_BIO || 'Computer Science PhD Student', // 作者简介
+  BIO: process.env.NEXT_PUBLIC_BIO || '', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://notion.zhaozhan.site', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Notion, 博客', // 网站关键词 英文逗号隔开
   BLOG_FAVICON: process.env.NEXT_PUBLIC_FAVICON || '/favicon.ico', // blog favicon 配置, 默认使用 /public/favicon.ico，支持在线图片，如 https://img.imesong.com/favicon.png 
